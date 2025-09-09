4e85c8a8539279831fdb3591fcd1c63407a4601b
Gustavo Barboza <gustabarboza37@gmail.com>
2025-09-09 17:30:13 -0300

    Adicionado uma forma de receber os dados

---
+++ src/data/careerData.js
@@ -0,0 +1,61 @@
+export const careerExperiences = [
+  {
+    id: 1,
+    title: 'UX Designer',
+    period: 'Ago 2025 - Presente',
+    active: true, // Esse active é para ser o primeiro a mostrar quando renderizar
+    details: [
+      'Atuei com UX, edição de vídeos, design e SEO, focado em soluções criativas e otimização.',
+      'Ajudei sites a se destacarem no Google, utilizando análise de dados, SEMrush e Google Sheets para otimizar estratégias e melhorar resultados.',
+      'Desenvolvi habilidades em análise e otimização de dados que são diretamente aplicáveis ao desenvolvimento de software.',
+    ],
+    skills: ['UX', 'Design', 'Edição de Vídeo', 'SEO'],
+  },
+  {
+    id: 2,
+    title: 'Independent Software Developer',
+    period: 'Jan 2025 - Jul 2025',
+    active: false,
+    details: [
+      'Desenvolvi websites personalizados, desde o layout até a implementação de funcionalidades, usando JavaScript, Node.js e React.',
+      'Criei e gerenciei sites em WordPress, incluindo a personalização de layouts e o uso de plugins para criar soluções de e-commerce.',
+      'Gerenciei a configuração de domínio, hospedagem e segurança, além de integrar diferentes serviços e APIs.',
+      'Foco em entregar projetos otimizados para usabilidade e performance, alinhados com os objetivos de negócio dos clientes.',
+    ],
+    skills: ['JavaScript', 'Node.js', 'React', 'WordPress'],
+  },
+  {
+    id: 3,
+    title: 'Design, Editor de Vídeo e SEO',
+    period: 'Jan 2024 - Fev 2025',
+    active: false,
+    details: [
+      'Atuei com UX, edição de vídeos, design e SEO, focado em soluções criativas e otimização.',
+      'Ajudei sites a se destacarem no Google, utilizando análise de dados, SEMrush e Google Sheets para otimizar estratégias e melhorar resultados.',
+      'Desenvolvi habilidades em análise e otimização de dados que são diretamente aplicáveis ao desenvolvimento de software.',
+    ],
+    skills: ['UX', 'Design', 'Edição de Vídeo', 'SEO'],
+  },
+  {
+    id: 4,
+    title: 'Designer',
+    period: 'Jun 2021 - Ago 2021',
+    active: false,
+    details: [
+      'Desenvolvi projetos de design para clientes da empresa.',
+      'Usei programas com Photoshop e Illustrator para criar artes para redes sociais como Instagram e YouTube.',
+    ],
+    skills: ['Photoshop', 'Illustrator', 'Design Gráfico'],
+  },
+  {
+    id: 5,
+    title: 'Design e Editor de Vídeo',
+    period: 'Abr 2021 - Set 2021',
+    active: false,
+    details: [
+      'Criei conteúdo de marketing digital para diversas plataformas (Instagram, Youtube, etc.).',
+      'Produzi vídeos e editei imagens, aprimorando meu senso estético e atenção aos detalhes, competências essenciais para o desenvolvimento Front-end.',
+    ],
+    skills: ['Design', 'Edição de Vídeo', 'Marketing Digital'],
+  },
+];
