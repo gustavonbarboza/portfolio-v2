c338974a3e8ead0b941a05e8668896da600e78d9
Gustavo Barboza <gustabarboza37@gmail.com>
2025-09-10 11:57:41 -0300

    Adicionado alguns estilos a mais de cores

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -3,6 +3,7 @@ export default {
   secondary: '#CCD6F6',
 
   background: '#0A192F',
+  secondaryBackground: '#1A414D',
   surface: '#1E1E1E',
 
   text: {
@@ -17,5 +18,12 @@ export default {
     transparent03: 'rgba(90, 224, 196, 0.3)',
     transparent02: 'rgba(90, 224, 196, 0.2)',
     transparent01: 'rgba(90, 224, 196, 0.1)',
+  },
+  secondaryVariants: {
+    transparent05: 'rgba(204, 214, 246, 0.5)',
+    transparent04: 'rgba(204, 214, 246, 0.4)',
+    transparent03: 'rgba(204, 214, 246, 0.3)',
+    transparent02: 'rgba(204, 214, 246, 0.2)',
+    transparent01: 'rgba(204, 214, 246, 0.1)',
   },
 };
