c7edaebe6f3f3c963bdd3f8cabec14eec50041ee
Gustavo Barboza <gustabarboza37@gmail.com>
2025-09-10 16:33:10 -0300

    Adicionado o conteúdo do Projeto em Desenvolvimento

---
+++ src/data/projectDevelopingData.js
@@ -0,0 +1,23 @@
+import screenshot1 from '../assets/images/projetctDevelopingImages/screenshot1.png';
+import screenshot2 from '../assets/images/projetctDevelopingImages/screenshot2.png';
+import screenshot3 from '../assets/images/projetctDevelopingImages/screenshot3.png';
+import screenshot4 from '../assets/images/projetctDevelopingImages/screenshot4.png';
+import screenshot5 from '../assets/images/projetctDevelopingImages/screenshot5.png';
+
+export const projectDevelopingData = {
+  id: 1,
+  title: 'Sistema Uniser - UNB ',
+  details: 'O sistema da Uniser/UnB será uma plataforma digital que centralizará a gestão acadêmica, administrativa e financeira. Ele substituirá processos manuais, permitindo o cadastro de diversos perfis (alunos, professores, etc.), o controle de acesso, a gestão de frequência e a emissão de relatórios, tudo de forma acessível e em conformidade com a LGPD.',
+  highlight: 'Em deselvimento',
+  links: [
+    { type: 'demostracao', url: 'https://www.google.com' },
+    { type: 'repositorio', url: 'https://github.com/seu-usuario/seu-repositorio' },
+  ],
+  images: [
+    screenshot1,
+    screenshot2,
+    screenshot3,
+    screenshot4,
+    screenshot5,
+  ],
+};
